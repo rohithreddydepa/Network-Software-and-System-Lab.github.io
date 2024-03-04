c31b8a8d5bc2c22f0396af001acd199d3aa42347
Rohith Reddy Depa <144377504+Rohith-reddy-depa@users.noreply.github.com>
2024-03-03 22:58:53 -0600

    added teaching and header dropdown autoclose on small screen is added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,21 @@ import { Router } from "@angular/router";
 })
 export class HeaderComponent {
   constructor(public router: Router) {}
-
-  test = true;
+  menuItems = [
+    { name: "Home", link: "/" },
+    { name: "Team", link: "/team" },
+    { name: "Equipment", link: "/equipment" },
+    { name: "Research & Publications", link: "/publications" },
+    { name: "Grants", link: "/grants" },
+    { name: "Teaching", link: "/teaching" },
+    { name: "Seminars and talks", link: "/seminars" },
+  ];
+  closeDropdown() {
+    let element: HTMLElement = document.querySelector(
+      ".navbar-toggler",
+    ) as HTMLElement;
+    if (element.getAttribute("aria-expanded") === "true") {
+      element.click();
+    }
+  }
 }
